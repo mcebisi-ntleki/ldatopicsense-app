15a59962efb4e42bdbe0f786562336a1ae65549e
Mcebisi Ntleki <51955747+mcebisi-ntleki@users.noreply.github.com>
2025-05-21 16:01:45 +0100

    Update app.py
    Revised NLTK code for deployment in Streamlit Community Cloud

--- app.py
+++ app.py
@@ -30,6 +30,17 @@ from textblob import TextBlob
 # Load environment variables
 load_dotenv()
 
+# For deployment on Streamlit Community Cloud
+nltk_data_path = os.path.join(os.path.dirname(__file__), 'nltk_data')
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+
 # Download necessary NLTK data
 try:
     nltk.data.find('tokenizers/punkt')
